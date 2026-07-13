ffd2d9912e1677855d07feeb8a807c22fde640e3
buli <hvlrcb@gmail.com>
2026-07-13 18:51:27 +0700

    Add files via upload

---
+++ app/src/main/java/com/wallettemplate/WalletSettingsActivity.java
@@ -0,0 +1,9 @@
+package com.wallettemplate;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class WalletSettingsActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.activity_wallet_settings);
+    }
+}
