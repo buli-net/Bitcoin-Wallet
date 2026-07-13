aeacd91361101778794f62700c19026b5be61d96
buli <hvlrcb@gmail.com>
2026-07-13 20:08:52 +0700

    Add files via upload

--- app/src/main/java/com/example/wallettemplate/WalletApplication.kt
+++ app/src/main/java/com/example/wallettemplate/WalletApplication.kt
@@ -1,27 +1,3 @@
-
 package com.example.wallettemplate
-
 import android.app.Application
-import org.bitcoinj.base.BitcoinNetwork
-import org.bitcoinj.base.ScriptType
-import org.bitcoinj.kits.WalletAppKit
-import java.io.File
-
-class WalletApplication : Application() {
-    lateinit var walletAppKit: WalletAppKit
-    val network = BitcoinNetwork.REGTEST
-    val scriptType = ScriptType.P2WPKH
-    override fun onCreate() {
-        super.onCreate()
-        val dir = File(filesDir, "wallet")
-        dir.mkdirs()
-        walletAppKit = object : WalletAppKit(network, scriptType, dir, "wallettemplate") {
-            override fun onSetupCompleted() {
-                wallet().allowSpendingUnconfirmedTransactions()
-            }
-        }
-        walletAppKit.setAutoSave(true)
-        walletAppKit.startAsync()
-        walletAppKit.awaitRunning()
-    }
-}
+class WalletApplication: Application()
