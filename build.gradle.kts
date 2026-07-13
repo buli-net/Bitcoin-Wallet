69ae2e2a34b496da87ed961dcb36cc438d165641
buli <hvlrcb@gmail.com>
2026-07-13 20:23:14 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,1 @@
-plugins { id("com.android.application") version "8.2.2" apply false; id("org.jetbrains.kotlin.android") version "1.9.22" apply false }
+// Root build - plugins versions are defined in app/build.gradle.kts
