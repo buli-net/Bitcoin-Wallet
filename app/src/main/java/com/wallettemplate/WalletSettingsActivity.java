90c65938a3b24baee3558294d3d4aafe806004a5
buli <hvlrcb@gmail.com>
2026-07-13 19:26:23 +0700

    Delete app/src/main/java/com/wallettemplate directory

--- app/src/main/java/com/wallettemplate/WalletSettingsActivity.java
+++
@@ -1,9 +0,0 @@
-package com.wallettemplate;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-public class WalletSettingsActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle b){
-        super.onCreate(b);
-        setContentView(R.layout.activity_wallet_settings);
-    }
-}
